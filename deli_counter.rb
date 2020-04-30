8f98ffdeda8765c404136b4a76a621a4864c1cda
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-30 18:07:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,12 +13,12 @@ end
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}.You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving (array)
   if array.empty?
-   puts "There is nobody waiting to be served!"
+   puts "The line is empty!"
  else
    puts "Currently serving #{array[0]}"
    array.shift
