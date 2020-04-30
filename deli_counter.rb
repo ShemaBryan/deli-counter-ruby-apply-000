3cb211397dc4e0b0343f34b1b10c3425e5b00c41
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-30 17:59:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ katz_deli = []
 
 def line (katz_deli)
 line_array = []
-if katz_deli.length == 0 
+if katz_deli.length == 0
   puts "The line is currently empty."
 else
-  katz_deli.each do |name, index|
+  katz_deli.each.with_index(1) do |name, index|
   line_array.push ("#{index}. #{name}")
 end
   puts "The line is currently: #{line_array.join (" ")}"
