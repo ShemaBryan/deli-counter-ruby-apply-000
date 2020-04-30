244a703480dcab1d9715631d9873cef66a16e42e
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-30 16:17:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number (array, element)
 end
 
 def now_serving (katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
    puts "There is nobody waiting to be served!"
  else
    puts "Currently serving #{katz_deli.first}"
