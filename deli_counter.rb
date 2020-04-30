9347ade7e0d0705665873d76d3211eea8533c412
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-30 17:47:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}.You are #{array}"
+  puts "Welcome, #{name}.You are number #{array}"
 end
 
 def now_serving (katz_deli)
